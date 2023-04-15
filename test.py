ccabb061b66515ef65ea412c4102ce4e2c1ee2fe
bambi <bambi425sl@gmail.com>
2023-04-15 17:40:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-<<<<<<< Updated upstream
 # write a program to print hello world
 print("Hello World")
-=======
-# function composition
-def make_adder(n):
-  def adder(x):
-      return x + n
-  return adder
-
-def square(x):
-  return x * x
-
-def triple(x):
-  return 3 * x
-
-def composel(f, g):
-  def h(x):
-    return f(g(x))
-  return h
->>>>>>> Stashed changes
