c664f3b0a14f08c564e028944fc37ca3f239b3d6
BambiSheng <115133376+BambiSheng@users.noreply.github.com>
2023-04-15 09:22:18 +0000

    test

--- test.py
+++ test.py
@@ -1,16 +1,2 @@
-# function composition
-def make_adder(n):
-  def adder(x):
-      return x + n
-  return adder
-
-def square(x):
-  return x * x
-
-def triple(x):
-  return 3 * x
-
-def composel(f, g):
-  def h(x):
-    return f(g(x))
-  return h
+# write a program to print hello world
+print("Hello World")
